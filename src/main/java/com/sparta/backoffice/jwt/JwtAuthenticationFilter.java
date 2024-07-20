d7e490b0db393e6b3364973442ff9cf6ff4e146a
won <januse159@naver.com>
2024-07-20 13:49:35 +0900

    로그인 기능 구현

--- src/main/java/com/sparta/backoffice/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/sparta/backoffice/jwt/JwtAuthenticationFilter.java
@@ -9,6 +9,8 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -30,12 +32,12 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
         log.info("로그인 시도");
         try {
-            LoginRequestDto requestDTO = new ObjectMapper().readValue(request.getInputStream(), LoginRequestDto.class);
+            LoginRequestDto requestDto = new ObjectMapper().readValue(request.getInputStream(), LoginRequestDto.class);
 
             return getAuthenticationManager().authenticate(
                     new UsernamePasswordAuthenticationToken(
-                            requestDTO.getEmail(),
-                            requestDTO.getPassword(),
+                            requestDto.getEmail(),
+                            requestDto.getPassword(),
                             null
                     )
             );
